e1cfcd3ea09e4337d3def8d981bd0dfc91439b8f
Joemarie <jojodenoy@gmail.com>
2023-03-01 18:38:08 +0800

    Added information for my Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
+        job1._jobTitle = "Salesman";
+        job1._company = "Ryans Apparel";
         job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._endYear = 2021;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
+        job2._jobTitle = "Utility";
+        job2._company = "RKJ General Services & Sales Inc.";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
